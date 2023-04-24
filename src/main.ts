b0011f998764e77b6771691e6d73a556058dc03b
chovrio <1480733667@qq.com>
2023-04-24 20:56:20 +0800

    feat: catch all exceptions and HTTP exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/hhtp.exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 捕获所有异常和HTTP异常
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
